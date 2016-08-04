1155bcd156ecf6005fdba16f8c90b71e2ab8dfab
Juan I. Benavides <linnk87@gmail.com>
2016-08-04 10:59:52 -0500

    Extracting the ego, adding the Allman and a more useful README.md

--- Allman/Sniffs/ControlStructures/ControlSignatureSniff.php
+++ Allman/Sniffs/ControlStructures/ControlSignatureSniff.php
@@ -7,7 +7,7 @@
  * @category  PHP
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
- * @copyright 2006-2014 Linnk Pty Ltd (ABN 77 084 670 600)
+ * @copyright 2006-2014 Allman Pty Ltd (ABN 77 084 670 600)
  * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
@@ -18,12 +18,12 @@
  * @category  PHP
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
- * @copyright 2006-2014 Linnk Pty Ltd (ABN 77 084 670 600)
+ * @copyright 2006-2014 Allman Pty Ltd (ABN 77 084 670 600)
  * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  * @version   Release: @package_version@
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
-class Linnk_Sniffs_ControlStructures_ControlSignatureSniff implements PHP_CodeSniffer_Sniff
+class Allman_Sniffs_ControlStructures_ControlSignatureSniff implements PHP_CodeSniffer_Sniff
 {
 
     /**
--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# Allman CodeSniffer & Linnk Standards
+# Allman CodeSniffer
 
-This is the beginning of a dark era.
+This is kind of a continuation from my previous fixer [PHP_Allman](https://github.com/Linnk/PHP-Allman), but this time is based on [PHP_CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer), which is more powerful and flexible in my opinion.
+
+The principles are the same, tho:
+
+1. I believe in [Allman indent style](https://en.wikipedia.org/wiki/Indent_style#Allman_style).
+2. I believe in tabs instead of spaces.
+
+The good thing now, is that if you don't like one of those you can customize it easier.
+
+# Installation for development
+
+Clone it:
+
+```
+$ git clone https://github.com/Linnk/Allman_CodeSniffer.git
+```
+
+Compose it:
+
+```
+$ composer install
+```
+
+Usage:
+
+```
+$ vendor/bin/phpcs --standard=Allman /path/to/your/code
+```
+
+That simple.
