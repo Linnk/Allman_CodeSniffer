774d2a74342c6777c5b823aa7a913459e1cf20fa
Juan I. Benavides <linnk87@gmail.com>
2016-09-01 05:30:34 -0500

    Block comment align and style.

---
+++ Allman/Sniffs/Commenting/StyleBlockCommentSniff.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Ensures correct indentation for doc blocks.
+ */
+class Allman_Sniffs_Commenting_StyleBlockCommentSniff implements PHP_CodeSniffer_Sniff
+{
+	/**
+	 * Returns an array of tokens this test wants to listen for.
+	 *
+	 * @return array
+	 */
+	public function register()
+	{
+		return array(T_DOC_COMMENT_STAR, T_DOC_COMMENT_CLOSE_TAG);
+	}
+
+	/**
+	 * Processes this test, when one of its tokens is encountered.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+	 * @param int                  $stackPtr  The position of the current token in the
+	 *                                 stack passed in $tokens.
+	 * @return void
+	 */
+	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+	{
+		$tokens = $phpcsFile->getTokens();
+
+		$open = $phpcsFile->findPrevious(T_DOC_COMMENT_OPEN_TAG, $stackPtr);
+
+		if ($tokens[$stackPtr]['type'] === 'T_DOC_COMMENT_CLOSE_TAG')
+		{
+			if ($tokens[$stackPtr]['content'] !== '*/')
+			{
+				$error = 'Expected */ as comment close tag; found “%s”.';
+				$data = array($tokens[$stackPtr]['content']);
+
+				if ($phpcsFile->addFixableError($error, $stackPtr, 'SimpleCommentCloseTag', $data))
+				{
+					$phpcsFile->fixer->replaceToken($stackPtr, '*/');
+				}
+			}
+		}
+
+		$before = $stackPtr - 1;
+		if ($tokens[$before]['type'] !== 'T_DOC_COMMENT_WHITESPACE')
+		{
+			return;
+		}
+
+		$base = str_repeat("\t", round(substr_count($tokens[$open - 1]['content'], ' ') / 4));
+
+		$raw = isset($tokens[$before]['orig_content']) ? $tokens[$before]['orig_content'] : $tokens[$before]['content'];
+
+		if ($raw !== $base.' ')
+		{
+			$error = 'Indent expected as “%s”; found “%s”.';
+			$data = array(
+				str_replace(array("\n", "\t"), array('\n', '\t'), $base.' '),
+				str_replace(array("\n", "\t"), array('\n', '\t'), $raw),
+			);
+			print_r($tokens[$before]['type']);
+			print_r($tokens[$before]['content']);
+
+			if ($phpcsFile->addFixableError($error, $before, 'WrongBlockCommentIndent', $data))
+			{
+				if ($tokens[$before]['content'] === "\n")
+				{
+					$phpcsFile->fixer->addContentBefore($stackPtr, " ");
+				}
+				else
+				{
+					$phpcsFile->fixer->replaceToken($before, $base.' ');
+				}
+			}
+		}
+	}
+}
